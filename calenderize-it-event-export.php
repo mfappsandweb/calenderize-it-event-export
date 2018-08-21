8604505f90fec2abdf6d6a8696375cdf2040b59a
mfsoftworks <it@nygmarosebeauty.com>
2018-08-21 17:29:12 +0800

    [core] core plugin file created.

---
+++ calenderize-it-event-export.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin Name:    Calenderize It Event Export
+ * Description:    Export Calenderize It events into HTML file.
+ * Version:        0.1.1
+ * Author:         MF Softworks
+ * Author URI:     https://mf.nygmarosebeauty.com/
+ * License:        GPLv3
+ * Copyright:      MF Softworks
+ * 
+ */
+
+ 
+define( 'Calenderize_It_Event_Export_Version', '0.1.1' );
+
+class Calenderize_It_Export_Events {
+
+    /**
+     * Options array for callbacks
+     */
+    private $options;
+
+    /**
+     * Construct the export event class
+     */
+    public function __construct() {
+
+    }
+
+    /**
+     * Add export page
+     */
+    public function create_admin_page() {
+
+    }
+
+    /**
+     * Build HTML file of events
+     */
+    private function build_event_html() {
+
+    }
+}
+?>
